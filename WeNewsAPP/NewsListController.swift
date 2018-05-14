dfe56be02f60a60eaacd01009693c48f4606729a
mlch911 <mlch911@mluoc.tech>
2018-05-14 15:05:30 +0800

    Ver 0.0.5

--- WeNewsAPP/NewsListController.swift
+++ WeNewsAPP/NewsListController.swift
@@ -7,72 +7,40 @@
 //
 
 import UIKit
-import CoreData
 
-class NewsListController: UITableViewController, NSFetchedResultsControllerDelegate {
+
+class NewsListController: UITableViewController {
 
     var newsList: [Post] = []
     var parentNavi: UINavigationController?
     var id = 0
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    var posts: [PostMO] = []
-    var fc: NSFetchedResultsController<PostMO>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        fetchFromNetwork()
+        getData()
         
         refreshControl = UIRefreshControl()
         
         tableView.estimatedRowHeight = 100
         tableView.rowHeight = UITableViewAutomaticDimension
         
-        refreshControl?.addTarget(self, action: #selector(fetchFromNetwork), for: .valueChanged)
-        NotificationCenter.default.addObserver(self, selector: #selector(fetchFromNetwork), name: NotificationHelper.updateList, object: nil)
+        refreshControl?.addTarget(self, action: #selector(getData), for: .valueChanged)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NotificationHelper.updateList, object: nil)
     }
     
-    func fetchAllData() {
-        let request: NSFetchRequest<PostMO> = PostMO.fetchRequest()
-        let sd = NSSortDescriptor(key: "id", ascending: true)
-        request.sortDescriptors = [sd]
-        let context = appDelegate.persistentContainer.viewContext
-        fc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
-        fc.delegate = self
-        do {
-            try fc.performFetch()
-            if let objectcs = fc.fetchedObjects {
-                posts = objectcs
-            }
-        } catch {
-            print(error)
-        }
-    }
-    
-    @objc func fetchFromNetwork() {
+    @objc func getData() {
         //获取对应分类文章列表
         //通过id采集到的文章
-        posts = []
         Post.request(id: id) { (posts) in
             //如果文章有值，if true
             if let posts = posts {
                 OperationQueue.main.addOperation { // 使他们处于同一进程
-//                    self.posts = posts
-//                    self.tableView.reloadData() //重新加载
-//                    print("网络请求成功")
-//                    self.refreshControl?.endRefreshing()
-                    for post in posts {
-                        let postMO = PostMO(context: self.appDelegate.persistentContainer.viewContext)
-                        
-                    }
-                    self.posts = posts.map {
-                        let postMO = PostMO(context: self.appDelegate.persistentContainer.viewContext)
-                        postMO.id = Int16($0.id)
-                        postMO.title = $0.title
-                        postMO.url = $0.url
-                        postMO.content = $0.content
-                        postMO.relationship
-                    }
+                    self.newsList = posts
+                    self.tableView.reloadData() //重新加载
+                    print("网络请求成功")
+                    self.refreshControl?.endRefreshing()
                 }
             } else {
                 print("网络错误")
@@ -97,9 +65,9 @@ class NewsListController: UITableViewController, NSFetchedResultsControllerDeleg
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TextCell", for: indexPath) as! TextCell
 
-        let news = posts[indexPath.row]
+        let news = newsList[indexPath.row]
         cell.titleLabel.text = news.title
-        cell.commentLabel.text = "评论：\(news.comment_count)"
+        cell.commentLabel.text = "评论：\(news.comment_count!)"
 
         return cell
     }
--- WeNewsAPP/ViewController.swift
+++ WeNewsAPP/ViewController.swift
@@ -9,104 +9,70 @@
 import UIKit
 import Moya
 import PageMenu
-import CoreData
 
-class ViewController: UIViewController, NSFetchedResultsControllerDelegate {
+class ViewController: UIViewController {
     
     var pageMenu: CAPSPageMenu!
     var controllers: [UIViewController] = []
     let statusBarFrame = UIApplication.shared.statusBarFrame
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    var cates: [CategoryMO] = []
-    var fc: NSFetchedResultsController<CategoryMO>!
     
 //    @IBOutlet weak var resultLabel: UILabel!
 //
 //    @IBAction func cateBtnTap(_ sender: UIButton) {
 //    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        UINavigationBar.appearance().isHidden = true
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
-        fetchAllData()
-        initPageMenu()
-        DispatchQueue.init(label: "tech.mluoc.queueBackground", qos: .background, attributes: .concurrent).async {
-            self.fetchFromNetwork()
-        }
-    }
-    
-    func fetchFromNetwork(){
+    func showMenu(){
         Category.request { (cates) in
+            
             //创建分类菜单，以及每个导航栏的view
             //创建每个分类的viewcontroller
-            self.cates = []
-            for cate in cates! {
-                let cateMO = CategoryMO(context: self.appDelegate.persistentContainer.viewContext)
-                cateMO.count = Int16(cate.count)
-                cateMO.id = Int16(cate.id)
-                cateMO.title = cate.title
-                self.cates.append(cateMO)
+            self.controllers = cates!.map {
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "SBID_NEWSLIST") as! NewsListController
+                vc.title = $0.title
+                vc.id = $0.id
+                vc.parentNavi = self.navigationController
+                return vc
             }
-            self.appDelegate.saveContext()
-        }
-    }
-    
-    func fetchAllData() {
-        let request: NSFetchRequest<CategoryMO> = CategoryMO.fetchRequest()
-        let sd = NSSortDescriptor(key: "id", ascending: true)
-        request.sortDescriptors = [sd]
-        let context = appDelegate.persistentContainer.viewContext
-        fc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
-        fc.delegate = self
-        do {
-            try fc.performFetch()
-            if let objectcs = fc.fetchedObjects {
-                cates = objectcs
-            }
-        } catch {
-            print(error)
-        }
-    }
-    
-    func initPageMenu() {
-        //设置每个PageMenu属性
-        self.controllers = cates.map {
-            let vc = self.storyboard?.instantiateViewController(withIdentifier: "SBID_NEWSLIST") as! NewsListController
-            vc.title = $0.title
-            vc.id = Int($0.id)
-            vc.parentNavi = self.navigationController
-            return vc
-        }
-        let param: [CAPSPageMenuOption] = [
-            .menuItemSeparatorWidth(4.3),
-            .scrollMenuBackgroundColor(UIColor.white),
-            .viewBackgroundColor(UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)),
-            .bottomMenuHairlineColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 0.1)),
-            .selectionIndicatorColor(UIColor(red: 242/255, green: 116/255, blue: 119/255, alpha: 1)),
-            .menuMargin(20.0),
-            .menuHeight(40.0),
-//            .selectedMenuItemLabelColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
-            .selectedMenuItemLabelColor(UIColor(red: 242/255, green: 116/255, blue: 119/255, alpha: 1)),
-            .unselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
-            .menuItemFont(UIFont(name: "HelveticaNeue-Medium", size: 17.0)!),
-            .useMenuLikeSegmentedControl(true),
-            .menuItemSeparatorRoundEdges(true),
-            .selectionIndicatorHeight(2.0),
-            .menuItemSeparatorPercentageHeight(0.1),
-            .menuItemWidth(80)
-        ]
-        //menu的位置
-        let frame = CGRect(x: 0, y: self.statusBarFrame.height + 44, width: self.view.frame.width, height: self.view.frame.height - 44 - self.statusBarFrame.height)
-        self.pageMenu = CAPSPageMenu(viewControllers: self.controllers, frame: frame, pageMenuOptions: param)
+            //设置每个PageMenu属性
+            let param: [CAPSPageMenuOption] = [
+                .menuItemSeparatorWidth(4.3),
+                .scrollMenuBackgroundColor(UIColor.white),
+                .viewBackgroundColor(UIColor(red: 247.0/255.0, green: 247.0/255.0, blue: 247.0/255.0, alpha: 1.0)),
+                .bottomMenuHairlineColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 0.1)),
+                .selectionIndicatorColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
+                .menuMargin(20.0),
+                .menuHeight(40.0),
+                .selectedMenuItemLabelColor(UIColor(red: 18.0/255.0, green: 150.0/255.0, blue: 225.0/255.0, alpha: 1.0)),
+                .unselectedMenuItemLabelColor(UIColor(red: 40.0/255.0, green: 40.0/255.0, blue: 40.0/255.0, alpha: 1.0)),
+                .menuItemFont(UIFont(name: "HelveticaNeue-Medium", size: 17.0)!),
+                .useMenuLikeSegmentedControl(true),
+                .menuItemSeparatorRoundEdges(true),
+                .selectionIndicatorHeight(2.0),
+                .menuItemSeparatorPercentageHeight(0.1),
+                .menuItemWidth(80)
+            ]
+            //menu的位置
+            let frame = CGRect(x: 0, y: self.statusBarFrame.height + 44, width: self.view.frame.width, height: self.view.frame.height - 44 - self.statusBarFrame.height)
+            self.pageMenu = CAPSPageMenu(viewControllers: self.controllers, frame: frame, pageMenuOptions: param)
 //            self.pageMenu = CAPSPageMenu(viewControllers: self.controllers, frame: frame, pageMenuOptions: [])
-        
-        //子view加入到父view中
-        self.view.addSubview(self.pageMenu.view)
+            
+            //子view加入到父view中
+            self.view.addSubview(self.pageMenu.view)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
+        showMenu()
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
 }
+//test
